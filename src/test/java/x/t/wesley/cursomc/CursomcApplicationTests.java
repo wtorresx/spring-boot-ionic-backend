c72d4a79082c33d437efaa6f8cfcb4519461e171
Wesley Xavier <wxavier@outlook.com.br>
2019-11-29 18:38:09 -0300

    Projeto criado

---
+++ src/test/java/x/t/wesley/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package x.t.wesley.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
