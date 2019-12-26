3443ee67d49d51dcf69404b0b41f1963ab15ed17
Wesley Xavier <wxavier@outlook.com.br>
2019-12-26 14:51:01 -0300

    Validação customizada de CPF ou CNPJ na inserção de Cliente

--- src/main/java/x/t/wesley/cursomc/CursomcApplication.java
+++ src/main/java/x/t/wesley/cursomc/CursomcApplication.java
@@ -64,7 +64,7 @@ public class CursomcApplication implements CommandLineRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
-
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
---
+++ src/main/java/x/t/wesley/cursomc/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,41 @@
+package x.t.wesley.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import x.t.wesley.cursomc.domain.enums.TipoCliente;
+import x.t.wesley.cursomc.dto.ClienteNewDTO;
+import x.t.wesley.cursomc.resources.exceptions.FieldMessage;
+import x.t.wesley.cursomc.services.validation.utils.BR;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO clienteNewDTO, ConstraintValidatorContext context) {
+		List<FieldMessage> errors = new ArrayList<>();
+
+		// inclua os testes aqui, inserindo erros na errorsa
+		
+		if(clienteNewDTO.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(clienteNewDTO.getCpfOuCnpj())) {
+			errors.add(new FieldMessage("cpfOuCnpj", "CPF Inválido"));
+		}
+		
+		if(clienteNewDTO.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(clienteNewDTO.getCpfOuCnpj())) {
+			errors.add(new FieldMessage("cpfOuCnpj", "CNPJ Inválido"));
+		}
+		
+
+		for (FieldMessage e : errors) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return errors.isEmpty();
+	}
+}
