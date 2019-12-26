174d9204035dad27184ab619ef1315f1f0a1d325
Wesley Xavier <wxavier@outlook.com.br>
2019-12-26 13:09:07 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/x/t/wesley/cursomc/resources/ClienteResource.java
+++ src/main/java/x/t/wesley/cursomc/resources/ClienteResource.java
@@ -1,13 +1,27 @@
 package x.t.wesley.cursomc.resources;
 
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import x.t.wesley.cursomc.domain.Cliente;
+import x.t.wesley.cursomc.dto.ClienteDTO;
 import x.t.wesley.cursomc.services.ClienteService;
 
 @RestController
@@ -18,14 +32,60 @@ public class ClienteResource {
 	private ClienteService cliServ;
 
 	@GetMapping
-	public ResponseEntity<?> Clientes() {
-		return ResponseEntity.ok().body(cliServ.getClientes());
+	public ResponseEntity<List<ClienteDTO>> Clientes() {
+		List<Cliente> clientes = cliServ.getClientes();
+
+		List<ClienteDTO> clientesDTO = clientes.stream().map(cliente -> new ClienteDTO(cliente))
+				.collect(Collectors.toList());
+
+		return ResponseEntity.ok().body(clientesDTO);
+	}
+	
+	@GetMapping(value="/page")
+	public ResponseEntity<Page<ClienteDTO>> ClientesPage(
+			@RequestParam(value="page", defaultValue = "0") Integer page,
+			@RequestParam(value="linesPerPage", defaultValue = "24") Integer linesPerPage,
+			@RequestParam(value="orderBy", defaultValue = "nome") String orderBy,
+			@RequestParam(value="direction", defaultValue = "ASC") String direction) {
+		
+		Page<Cliente> clientes = cliServ.getClientesPage(page, linesPerPage, orderBy, direction);
+		Page<ClienteDTO> clientesDTO = clientes.map(cliente -> new ClienteDTO(cliente));
+		return ResponseEntity.ok().body(clientesDTO);
 	}
 
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Cliente> Cliente(@PathVariable("id") Integer id) {
-		Cliente categoria = cliServ.getCliente(id);
-		return ResponseEntity.ok().body(categoria);
+		Cliente cliente = cliServ.getCliente(id);
+		return ResponseEntity.ok().body(cliente);
 	}
+		
+	@PostMapping
+	public ResponseEntity<Void> novaCliente(@Valid @RequestBody ClienteDTO clienteDTO) {
+		
+		Cliente cliente = cliServ.fromDTO(clienteDTO);		
+		cliente = cliServ.postCliente(cliente);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(cliente.getId())
+				.toUri();
+
+		return ResponseEntity.created(uri).build();
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Void> editarCliente(@Valid @RequestBody ClienteDTO clienteDTO, @PathVariable Integer id) {
+		
+		Cliente cliente = cliServ.fromDTO(clienteDTO);
+		
+		cliente.setId(id);
+		cliente = cliServ.putCliente(cliente);
+
+		return ResponseEntity.noContent().build();
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> deletarCliente(@PathVariable Integer id) {
+		cliServ.deleteCliente(id);
+		return ResponseEntity.noContent().build();
+	}
+	
 
 }
--- src/main/java/x/t/wesley/cursomc/services/ClienteService.java
+++ src/main/java/x/t/wesley/cursomc/services/ClienteService.java
@@ -4,10 +4,16 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import x.t.wesley.cursomc.domain.Cliente;
+import x.t.wesley.cursomc.dto.ClienteDTO;
 import x.t.wesley.cursomc.repositories.ClienteRepository;
+import x.t.wesley.cursomc.services.exceptions.DataIntegrityException;
 import x.t.wesley.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -18,6 +24,11 @@ public class ClienteService {
 
 	public List<Cliente> getClientes() {
 		return cliRep.findAll();
+	}
+
+	public Page<Cliente> getClientesPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return cliRep.findAll(pageRequest);
 	}
 
 	public Cliente getCliente(Integer id) {
@@ -32,12 +43,43 @@ public class ClienteService {
 		return cliente.orElse(null);
 	}
 
-	public void postCliente(Cliente cliente) {
-		cliRep.save(cliente);
+	public Cliente postCliente(Cliente cliente) {
+		// Garantir que será um novo registro!
+		cliente.setId(null);
+		return cliRep.save(cliente);
 	}
 
-	public void postClientes(List<Cliente> clientes) {
-		cliRep.saveAll(clientes);
+	public List<Cliente> postClientes(List<Cliente> clientes) {
+		return cliRep.saveAll(clientes);
+	}
+
+	public Cliente putCliente(Cliente cliente) {
+
+		// Garantir que a cliente existe
+		Cliente newCliente = getCliente(cliente.getId());
+
+		updateData(newCliente, cliente);
+
+		return cliRep.save(newCliente);
+	}
+
+	public void deleteCliente(Integer id) {
+		getCliente(id);
+
+		try {
+			cliRep.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas!");
+		}
+	}
+
+	public Cliente fromDTO(ClienteDTO clienteDTO) {
+		return new Cliente(clienteDTO.getId(), clienteDTO.getNome(), clienteDTO.getEmail(), null, null);
+	}
+	
+	private void updateData(Cliente newCliente, Cliente cliente) {
+		newCliente.setNome(cliente.getNome());
+		newCliente.setEmail(cliente.getEmail());
 	}
 
 }
