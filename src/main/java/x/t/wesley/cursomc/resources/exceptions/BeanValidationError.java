cff220021803795a017ffa13551e2857a772205a
Wesley Xavier <wxavier@outlook.com.br>
2019-12-26 12:33:19 -0300

    Personalizando o retorno da validação

---
+++ src/main/java/x/t/wesley/cursomc/resources/exceptions/BeanValidationError.java
@@ -0,0 +1,24 @@
+package x.t.wesley.cursomc.resources.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BeanValidationError extends StandardError {
+
+	private static final long serialVersionUID = 1L;
+	
+	private List<FieldMessage> errors = new ArrayList<>();
+		
+	public BeanValidationError(Integer status, String msg, Long timeStamp) {
+		super(status, msg, timeStamp);
+	}
+
+	public List<FieldMessage> getErrors() {
+		return errors;
+	}
+
+	public void addError(String fieldName, String message) {
+		errors.add(new FieldMessage(fieldName, message));
+	}
+	
+}
