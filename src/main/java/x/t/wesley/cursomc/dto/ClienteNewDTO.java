6a0662df90c2fe6eb260cad03250481499734594
Wesley Xavier <wxavier@belfort.com.br>
2020-01-28 12:52:33 -0300

    Tratando excessões adequadamente

--- src/main/java/x/t/wesley/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/x/t/wesley/cursomc/dto/ClienteNewDTO.java
@@ -26,6 +26,7 @@ public class ClienteNewDTO implements Serializable {
 	
 	@NotEmpty(message = "Preenchimento Obrigatório")
 	private String cpfOuCnpj;
+	
 	private Integer tipo;
 	
 	@NotEmpty(message = "Preenchimento Obrigatório")
